3da5e16d1745d671a5516903d2eceb35d6e14c59
Frank Odey <frankodey2@gmail.com>
2019-03-31 01:40:13 +0100

    Add existing file

---
+++ taskOne.py
@@ -0,0 +1,5 @@
+hours = input("Enter your hours of work: ")
+rate = input("Enter your rate: ")
+pay = float(hours) * float(rate)
+print('Your pay is: ' + str(pay))
+print('All payment of' + pay +'is made via wire transfer')
